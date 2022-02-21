fbac71753a4e8f3135fcf23bab232160ab10d41e
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-21 14:06:53 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioTresListaTres/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioTresListaTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int soma = 0;
+
+            for (int i = 1; i <= 500; i++)
+            {
+                if (i % 2 != 0 && i % 3 == 0)
+                    soma += i;
+            }
+
+            Console.WriteLine("A soma é: " + soma);
+
+            Console.ReadKey();
+        }
+    }
+}
